1c2a2f9e5519afc5a994c9e461aee19f90a7ab30
DuyDaoNgoc <kinbingo@gmail.com>
2025-07-16 09:16:31 +0700

    code header

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import "./style.sass";
+export function App() {
+  return (
+    <>
+      <h1 style={{ color: "red", fontSize: "32px" }}>Parcel React App</h1>
+      <p>
+        Edit <code className="reponsize">src/App.tsx</code> to get started!
+      </p>
+    </>
+  );
+}
